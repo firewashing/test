1745d697f72ab94d7f1316e5c9f5d66a3fb2f15a
firewashing <firewashing7@gmail.com>
2026-05-29 21:10:37 +0800

    第一次提交

---
+++ ```bash.py
@@ -0,0 +1,20 @@
+```bash
+git init
+```
+
+```bash
+git add .
+```
+
+```bash
+git commit -m "第一次提交"
+```
+
+```bash
+git branch -M main
+```
+
+```bash
+git remote add origin https://github.com/firewashing/test.git
+```bash
+git push -u origin main
